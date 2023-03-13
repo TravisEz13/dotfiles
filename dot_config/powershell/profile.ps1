afe58f0cbd0178de1b527136a354ca4671e1dc79
travis plunk <github@ez13.net>
2023-03-13 13:32:59 -0700

    split into module

--- dot_config/powershell/profile.ps1
+++ dot_config/powershell/profile.ps1
@@ -5,6 +5,40 @@ using namespace System.Management.Automation;
 $userProfile = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::UserProfile)
 $downloads = Join-Path $userProfile -ChildPath 'Downloads'
 
+if (!$global:profileCommandCache) {
+    $global:profileCommandCache = @{}
+}
+function Get-CommandFast {
+    param(
+        [parameter()]
+        [string]
+        $Name,
+        [Parameter()]
+        [CommandTypes]
+        $CommandType = [CommandTypes]::Application,
+        [bool]
+        $Cache = $false
+    )
+
+    if($Cache) {
+        $key = $CommandType.ToString()
+        if ($global:profileCommandCache.ContainsKey($key)) {
+            $commands = $global:profileCommandCache.$key
+        }
+        else {
+            $commands = Get-Command -CommandType $CommandType
+            $global:profileCommandCache[$key] = $commands
+        }
+
+
+        $result = $commands | Where-Object { $_.Name -like $Name }
+
+        return $result
+    } else {
+        return Get-Command -Name $Name
+    }
+}
+
 #Enable concise errorview for PS7 and up
 if ($psversiontable.psversion.major -ge 7) {
     $ErrorView = 'ConciseView'
@@ -54,9 +88,10 @@ if (!$IsWindows) {
                 Write-Verbose "Updating env: $_ "
                 Invoke-Expression $_
             }
+
             $env:PATH = "${pshome}:$env:PATH"
-            if(!(Get-Command brew -ErrorAction SilentlyContinue)){
-                /opt/homebrew/bin/brew shellenv  | foreach-object {
+            if (!(Get-CommandFast -Name brew)) {
+                /usr/local/Homebrew/bin/brew shellenv  | foreach-object {
                     Write-Verbose "Updating env: $_ "
                     Invoke-Expression $_
                 }
@@ -73,7 +108,7 @@ if (!$IsWindows) {
     {
         param([string]$command, [string]$missedMessage)
 
-        $c = Get-Command $command -ErrorAction Ignore
+        $c = Get-CommandFast $command
         if (-not $c) {
             if (-not [string]::IsNullOrEmpty($missedMessage))
             {
@@ -110,7 +145,8 @@ if (!$IsWindows) {
 # }
 
 #Enable new fancy progress bar
-if ([version]::new($psversiontable.psversion.major,$psversiontable.psversion.Minor,$psversiontable.psversion.Patch) -ge [version]'7.2.0') {
+$psVersion = [version]::new($psversiontable.psversion.major,$psversiontable.psversion.Minor,$psversiontable.psversion.Patch)
+if ($psVersion -ge [version]'7.2.0' -and $psVersion -lt [version]'7.3.0') {
     $featureToEnable = @(
         'PSAnsiRenderingFileInfo'
     )
@@ -131,7 +167,7 @@ if ([version]::new($psversiontable.psversion.major,$psversiontable.psversion.Min
 
 
 #Starship Prompt
-if (Get-Command starship -CommandType Application -ErrorAction SilentlyContinue) {
+if (Get-CommandFast -Name starship -CommandType Application) {
     #Separate Prompt for vscode. We don't use the profile so this works for both integrated and external terminal modes
     if ($ENV:VSCODE_GIT_IPC_HANDLE) {
         $ENV:STARSHIP_CONFIG = "$HOME\.config\starship-vscode.toml"
@@ -140,7 +176,6 @@ if (Get-Command starship -CommandType Application -ErrorAction SilentlyContinue)
     [string]$starshipPrompt = (& starship init powershell --print-full-init) -join "`n"
 
     #Kludge: Take a common line and add a suffix to it
-    $stubToReplace = 'prompt {'
     $replaceShim = {
         $env:STARSHIP_ENVVAR = if (Test-Path Variable:/PSDebugContext) {
             "`u{1f41e}"
@@ -215,11 +250,6 @@ Import-Module Microsoft.PowerShell.UnixCompleters -ErrorAction SilentlyContinue
 #region Start up
 
 
-if (Test-Path "/Applications/Remove Sophos Endpoint.app") {
-    # Since it's a .app, the best we can do is pop the GUI
-    open "/Applications/Remove Sophos Endpoint.app"
-}
-
 #endregion
 
 
@@ -233,30 +263,12 @@ $PSDefaultParameterValues += @{"Format-List:Force" = $true}
 $PSDefaultParameterValues += @{"Get-*:OutVariable" = "__"}
 $PSDefaultParameterValues += @{"Install-Module:Repository" = "PSGallery"}
 
-# Cleanup bad local repositories
-function Clear-PSRepository {
-    Get-PSRepository | ForEach-Object {
-        $uri = [uri] $_.SourceLocation
-        if ($uri.scheme -notlike 'http*' -and !(test-path $uri.OriginalString)) {
-            $_ | Unregister-PSRepository
-        }
-    }
-}
 
 #Set-PSReadlineOption -EditMode Windows
 if ($null -eq $IsWindows) {
     $IsWindows = $true
 }
 
-
-function Set-ConstrainedLanguageMode
-{
-    $ExecutionContext.SessionState.LanguageMode='ConstrainedLanguage'
-    if($Global:TabTitle)
-    {
-        Set-TabTitle -Title "Constrained - $Global:TabTitle"
-    }
-}
 
 function script:Get-CompletionResults {
     param(
@@ -376,267 +388,6 @@ function New-MultipassIntance
 }
 # git aliases
 
-git config --global alias.pushf "push --force-with-lease"
-git config --global alias.discard "checkout --"
-git config --global alias.logoneline "log --pretty=oneline"
-
-function Enable-DockerExperimentalCli
-{
-    $dockerConfigFolder = "$env:userprofile/.docker"
-    if(!(Test-Path $dockerConfigFolder)){ new-item -Type Directory -Path $dockerConfigFolder}
-    $dockerCliConfig = "$env:userprofile/.docker/config.json"
-    $dockerCliBackup = "$env:userprofile/.docker/config-backup.json"
-    if(Test-Path $dockerCliConfig) { copy-item $dockerCliConfig $dockerCliBackup -force}
-    @{experimental='enabled'}|ConvertTo-Json | Out-File -Encoding ascii -FilePath $dockerCliConfig
-}
-
-function Install-IfNotInstalled {
-    param(
-        [Parameter(Mandatory)]
-        [string] $PackageName,
-
-        [ValidateSet('brew', 'yarn')]
-        [string] $PackageManager = 'brew',
-
-        [string] $packageVersion
-    )
-
-    if (!(Get-Command $packageName -ErrorAction SilentlyContinue)) {
-        switch ($PackageManager) {
-            'brew' {
-                Write-Verbose "insalling $PackageName ..." -Verbose
-                brew install $PackageName
-            }
-            'yarn' {
-                $yarnPackage = $PackageName
-                if ($packageVersion) {
-                    $yarnPackage += "@$packageVersion"
-                }
-                Write-Verbose "insalling $yarnPackage ..." -Verbose
-                sudo yarn global add $yarnPackage
-            }
-        }
-    }
-}
-
-function Test-Spelling
-{
-    param(
-        [string[]]
-        $Paths,
-        [switch]
-        $Fix
-
-    )
-
-    if(!(Get-Command mdspell -ErrorAction SilentlyContinue))
-    {
-        Install-IfNotInstalled -packageName node -PackageManager brew
-        Install-IfNotInstalled -packageName yarn -PackageManager brew
-        Install-IfNotInstalled -packageName markdown-spellcheck -PackageManager yarn -packageVersion 0.11.0
-    }
-
-    $fileList = @()
-
-    foreach($path in $Paths)
-    {
-        if($path -match '^\.[/\\]')
-        {
-            $fileList += ($path -replace '^\.[/\\]')
-        }
-        else {
-            $fileList += $path
-        }
-    }
-
-    $extraParams = @()
-
-    if(!$Fix.IsPresent)
-    {
-        $extraParams += '--report'
-    }
-
-    Write-Verbose "Testing spelling for $fileList" -Verbose
-    mdspell $fileList --ignore-numbers --ignore-acronyms @extraParams --en-us --no-suggestions
-}
-
-function Clear-AzResourceGroup
-{
-    $null = Get-AzResourceGroup | ForEach-Object{
-        $yes=[System.Management.Automation.Host.ChoiceDescription]::new('&Yes')
-        $no=[System.Management.Automation.Host.ChoiceDescription]::new('&No')
-        $result = Read-Choice -message "Delete $($_.ResourceGroupName)" -choices $yes, $no -caption question -defaultChoiceIndex 1
-        if($result -eq '&Yes'){
-            $_
-        }
-    } | Remove-AzResourceGroup -Force -AsJob
-}
-
-function Get-ProcessPreventingSleep
-{
-    param(
-        [switch]
-        $PassThru
-    )
-    $results = pmset -g assertions |
-        Where-Object{$_ -match 'pid.*Prevent\w*Sleep'} |
-            ForEach-Object{
-                $null = $_ -match 'pid (\d*){1}.*(Prevent\w*Sleep)'
-                $processId = $Matches.1
-                $issue = $Matches.2
-                $process = Get-Process -Id $processId
-                $process | Add-Member -Name Issue -MemberType NoteProperty -Value $issue
-                Write-Output $process
-    }
-    if($PassThru) {
-        $results | ForEach-Object { Write-Output $_}
-        return
-    }
-
-    $results | Select-Object Id, ProcessName, Issue
-}
-
-function Get-PSGitCommit
-{
-    $sma = Get-Item (Join-Path $PSHome "System.Management.Automation.dll")
-    $formattedVersion = $sma.VersionInfo.ProductVersion
-    $formattedVersion
-}
-
-New-Alias -Name Cleanup-AzResourceGroup -Value Clear-AzResourceGroup -Force
-
-function ConvertTo-Base64
-{
-    param(
-        [parameter(Mandatory,ValueFromPipeline = $true)]
-        [string]$InputObject
-    )
-
-    return [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($InputObject))
-}
-
-function ConvertFrom-Base64
-{
-    param(
-        [parameter(Mandatory,ValueFromPipeline = $true)]
-        [string]$InputObject
-    )
-
-    return [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($InputObject))
-}
-
-function cdgit {
-    [CmdletBinding(DefaultParameterSetName='Path', HelpUri='https://go.microsoft.com/fwlink/?LinkID=2097049')]
-    param(
-        [switch]
-        ${PassThru}
-    )
-
-    DynamicParam {
-        $repos = Get-ChildItem -Path '~/git' -Directory | Select-Object -ExpandProperty Name
-
-        # Create the parameter attributs
-        $Attributes = [System.Collections.ObjectModel.Collection[System.Attribute]]::new()
-
-        $ParameterAttr = [System.Management.Automation.ParameterAttribute]::new()
-        $ParameterAttr.ParameterSetName = 'Path'
-        $ParameterAttr.ValueFromPipeline = $true
-        $ParameterAttr.ValueFromPipelineByPropertyName = $true
-        $ParameterAttr.Position = 0
-        $ParameterAttr.Mandatory = $Mandatory
-        $Attributes.Add($ParameterAttr) > $null
-
-        if($repos.Count -gt 0)
-        {
-            $ValidateSetAttr = [System.Management.Automation.ValidateSetAttribute]::new(([string[]]$repos))
-            $Attributes.Add($ValidateSetAttr) > $null
-        }
-
-        # Create the parameter
-        $Parameter = [System.Management.Automation.RuntimeDefinedParameter]::new("Path", [string], $Attributes)
-
-        # Return parameters dictionaly
-        $parameters = [System.Management.Automation.RuntimeDefinedParameterDictionary]::new()
-        $parameters.Add("Path", $Parameter) > $null
-        return $parameters
-    }
-
-    begin
-    {
-        try {
-            $Path = $PSBoundParameters["Path"]
-            $Path = Join-Path -Path '~/git' -ChildPath $Path
-        } catch {
-            throw
-        }
-    }
-
-    process
-    {
-        try {
-            Set-Location -Path $Path -PassThru:$PassThru
-        } catch {
-            throw
-        }
-    }
-}
-
-if(get-command github-copilot-cli -ErrorAction SilentlyContinue) {
-    function Invoke-CopilotCli {
-        [CmdletBinding(SupportsShouldProcess = $true)]
-        param(
-            [parameter(mandatory=$true, position=0)]
-            [string]
-            $Command,
-            [parameter(mandatory=$true, position=1, ValueFromRemainingArguments=$true)]
-            $Query
-        )
-
-        $shellOutFile = [system.io.path]::GetTempFileName() + '.ps1'
-        try {
-            github-copilot-cli $Command "$Query" --shellout $shellOutFile
-            if(Test-Path $shellOutFile) {
-                $script = Get-Content -Path $shellOutFile -Raw
-                $sb = [scriptblock]::create($script)
-                if ($PSCmdlet.ShouldProcess($sb.ToString(), 'Invoke Script') ) {
-                    & $sb
-                }
-            }
-        }
-        finally {
-            remove-item -Path $shellOutFile -ErrorAction SilentlyContinue -Force
-        }
-
-
-    }
-
-    function Invoke-CopilotGitAssist {
-        [Alias("git?")]
-        [CmdletBinding(SupportsShouldProcess = $true)]
-        param(
-            [parameter(mandatory=$true, position=0, ValueFromRemainingArguments=$true)]
-            $Query
-        )
-        Invoke-CopilotCli -Command git-assist -Query $Query
-    }
-
-    function Invoke-CopilotGHAssist {
-        [Alias("gh?")]
-        [CmdletBinding(SupportsShouldProcess = $true)]
-        param(
-            [parameter(mandatory=$true, position=0, ValueFromRemainingArguments=$true)]
-            $Query
-        )
-        Invoke-CopilotCli -Command gh-assist -Query $Query
-    }
-
-    function Invoke-CopilotWhatTheShell {
-        [Alias("wts","wts?")]
-        [CmdletBinding(SupportsShouldProcess = $true)]
-        param(
-            [parameter(mandatory=$true, position=0, ValueFromRemainingArguments=$true)]
-            $Query
-        )
-        Invoke-CopilotCli -Command what-the-shell -Query $Query
-    }
-}
+# git config --global alias.pushf "push --force-with-lease"
+# git config --global alias.discard "checkout --"
+# git config --global alias.logoneline "log --pretty=oneline"
---
+++ dot_local/share/powershell/Modules/MyProfileHelper/MyProfileHelper.psm1
@@ -0,0 +1,281 @@
+# Cleanup bad local repositories
+function Clear-PSRepository {
+    Get-PSRepository | ForEach-Object {
+        $uri = [uri] $_.SourceLocation
+        if ($uri.scheme -notlike 'http*' -and !(test-path $uri.OriginalString)) {
+            $_ | Unregister-PSRepository
+        }
+    }
+}
+
+function Set-ConstrainedLanguageMode
+{
+    $ExecutionContext.SessionState.LanguageMode='ConstrainedLanguage'
+    if($Global:TabTitle)
+    {
+        Set-TabTitle -Title "Constrained - $Global:TabTitle"
+    }
+}
+
+function Enable-DockerExperimentalCli
+{
+    $dockerConfigFolder = "$env:userprofile/.docker"
+    if(!(Test-Path $dockerConfigFolder)){ new-item -Type Directory -Path $dockerConfigFolder}
+    $dockerCliConfig = "$env:userprofile/.docker/config.json"
+    $dockerCliBackup = "$env:userprofile/.docker/config-backup.json"
+    if(Test-Path $dockerCliConfig) { copy-item $dockerCliConfig $dockerCliBackup -force}
+    @{experimental='enabled'}|ConvertTo-Json | Out-File -Encoding ascii -FilePath $dockerCliConfig
+}
+
+# Used at runtime
+function Install-IfNotInstalled {
+    param(
+        [Parameter(Mandatory)]
+        [string] $PackageName,
+
+        [ValidateSet('brew', 'yarn')]
+        [string] $PackageManager = 'brew',
+
+        [string] $packageVersion
+    )
+
+    if (!(Get-Command $packageName -ErrorAction SilentlyContinue)) {
+        switch ($PackageManager) {
+            'brew' {
+                Write-Verbose "insalling $PackageName ..." -Verbose
+                brew install $PackageName
+            }
+            'yarn' {
+                $yarnPackage = $PackageName
+                if ($packageVersion) {
+                    $yarnPackage += "@$packageVersion"
+                }
+                Write-Verbose "insalling $yarnPackage ..." -Verbose
+                sudo yarn global add $yarnPackage
+            }
+        }
+    }
+}
+
+function Test-Spelling
+{
+    param(
+        [string[]]
+        $Paths,
+        [switch]
+        $Fix
+
+    )
+
+    if(!(Get-Command mdspell -ErrorAction SilentlyContinue))
+    {
+        Install-IfNotInstalled -packageName node -PackageManager brew
+        Install-IfNotInstalled -packageName yarn -PackageManager brew
+        Install-IfNotInstalled -packageName markdown-spellcheck -PackageManager yarn -packageVersion 0.11.0
+    }
+
+    $fileList = @()
+
+    foreach($path in $Paths)
+    {
+        if($path -match '^\.[/\\]')
+        {
+            $fileList += ($path -replace '^\.[/\\]')
+        }
+        else {
+            $fileList += $path
+        }
+    }
+
+    $extraParams = @()
+
+    if(!$Fix.IsPresent)
+    {
+        $extraParams += '--report'
+    }
+
+    Write-Verbose "Testing spelling for $fileList" -Verbose
+    mdspell $fileList --ignore-numbers --ignore-acronyms @extraParams --en-us --no-suggestions
+}
+
+function Clear-AzResourceGroup
+{
+    [alias('Cleanup-AzResourceGroup')]
+    [CmdletBinding()]
+    param()
+    $null = Get-AzResourceGroup | ForEach-Object{
+        $yes=[System.Management.Automation.Host.ChoiceDescription]::new('&Yes')
+        $no=[System.Management.Automation.Host.ChoiceDescription]::new('&No')
+        $result = Read-Choice -message "Delete $($_.ResourceGroupName)" -choices $yes, $no -caption question -defaultChoiceIndex 1
+        if($result -eq '&Yes'){
+            $_
+        }
+    } | Remove-AzResourceGroup -Force -AsJob
+}
+
+function Get-ProcessPreventingSleep
+{
+    param(
+        [switch]
+        $PassThru
+    )
+    $results = pmset -g assertions |
+        Where-Object{$_ -match 'pid.*Prevent\w*Sleep'} |
+            ForEach-Object{
+                $null = $_ -match 'pid (\d*){1}.*(Prevent\w*Sleep)'
+                $processId = $Matches.1
+                $issue = $Matches.2
+                $process = Get-Process -Id $processId
+                $process | Add-Member -Name Issue -MemberType NoteProperty -Value $issue
+                Write-Output $process
+    }
+    if($PassThru) {
+        $results | ForEach-Object { Write-Output $_}
+        return
+    }
+
+    $results | Select-Object Id, ProcessName, Issue
+}
+
+function Get-PSGitCommit
+{
+    $sma = Get-Item (Join-Path $PSHome "System.Management.Automation.dll")
+    $formattedVersion = $sma.VersionInfo.ProductVersion
+    $formattedVersion
+}
+
+function ConvertTo-Base64
+{
+    param(
+        [parameter(Mandatory,ValueFromPipeline = $true)]
+        [string]$InputObject
+    )
+
+    return [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($InputObject))
+}
+
+function ConvertFrom-Base64
+{
+    param(
+        [parameter(Mandatory,ValueFromPipeline = $true)]
+        [string]$InputObject
+    )
+
+    return [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($InputObject))
+}
+
+
+function Set-GitLocation {
+    [alias('cdgit')]
+    [CmdletBinding(DefaultParameterSetName='Path', HelpUri='https://go.microsoft.com/fwlink/?LinkID=2097049')]
+    param(
+        [switch]
+        ${PassThru}
+    )
+
+    DynamicParam {
+        $repos = Get-ChildItem -Path '~/git' -Directory | Select-Object -ExpandProperty Name
+
+        # Create the parameter attributs
+        $Attributes = [System.Collections.ObjectModel.Collection[System.Attribute]]::new()
+
+        $ParameterAttr = [System.Management.Automation.ParameterAttribute]::new()
+        $ParameterAttr.ParameterSetName = 'Path'
+        $ParameterAttr.ValueFromPipeline = $true
+        $ParameterAttr.ValueFromPipelineByPropertyName = $true
+        $ParameterAttr.Position = 0
+        $ParameterAttr.Mandatory = $Mandatory
+        $Attributes.Add($ParameterAttr) > $null
+
+        if($repos.Count -gt 0)
+        {
+            $ValidateSetAttr = [System.Management.Automation.ValidateSetAttribute]::new(([string[]]$repos))
+            $Attributes.Add($ValidateSetAttr) > $null
+        }
+
+        # Create the parameter
+        $Parameter = [System.Management.Automation.RuntimeDefinedParameter]::new("Path", [string], $Attributes)
+
+        # Return parameters dictionaly
+        $parameters = [System.Management.Automation.RuntimeDefinedParameterDictionary]::new()
+        $parameters.Add("Path", $Parameter) > $null
+        return $parameters
+    }
+
+    begin
+    {
+        try {
+            $Path = $PSBoundParameters["Path"]
+            $Path = Join-Path -Path '~/git' -ChildPath $Path
+        } catch {
+            throw
+        }
+    }
+
+    process
+    {
+        try {
+            Set-Location -Path $Path -PassThru:$PassThru
+        } catch {
+            throw
+        }
+    }
+}
+
+function Invoke-CopilotCli {
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    param(
+        [parameter(mandatory=$true, position=0)]
+        [string]
+        $Command,
+        [parameter(mandatory=$true, position=1, ValueFromRemainingArguments=$true)]
+        $Query
+    )
+
+    $shellOutFile = [system.io.path]::GetTempFileName() + '.ps1'
+    try {
+        github-copilot-cli $Command "$Query" --shellout $shellOutFile
+        if(Test-Path $shellOutFile) {
+            $script = Get-Content -Path $shellOutFile -Raw
+            $sb = [scriptblock]::create($script)
+            if ($PSCmdlet.ShouldProcess($sb.ToString(), 'Invoke Script') ) {
+                & $sb
+            }
+        }
+    }
+    finally {
+        remove-item -Path $shellOutFile -ErrorAction SilentlyContinue -Force
+    }
+
+
+}
+
+function Invoke-CopilotGitAssist {
+    [Alias("git?")]
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    param(
+        [parameter(mandatory=$true, position=0, ValueFromRemainingArguments=$true)]
+        $Query
+    )
+    Invoke-CopilotCli -Command git-assist -Query $Query
+}
+
+function Invoke-CopilotGHAssist {
+    [Alias("gh?")]
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    param(
+        [parameter(mandatory=$true, position=0, ValueFromRemainingArguments=$true)]
+        $Query
+    )
+    Invoke-CopilotCli -Command gh-assist -Query $Query
+}
+
+function Invoke-CopilotWhatTheShell {
+    [Alias("wts","wts?")]
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    param(
+        [parameter(mandatory=$true, position=0, ValueFromRemainingArguments=$true)]
+        $Query
+    )
+    Invoke-CopilotCli -Command what-the-shell -Query $Query
+}
