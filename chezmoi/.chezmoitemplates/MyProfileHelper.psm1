0e8c9b428dcfce94bc71d77b9383096b68b17d5c
Travis Plunk <github@ez13.net>
2023-07-28 14:50:23 -0700

    add quick quickActions

--- chezmoi/.chezmoitemplates/MyProfileHelper.psm1
+++ chezmoi/.chezmoitemplates/MyProfileHelper.psm1
@@ -424,7 +424,7 @@ function Invoke-PingPath {
 
     Write-Progress -Activity $activityName -Status "Finding Path to $TargetName ..." -PercentComplete 0
     if ($IsMacOs) {
-        $trStrings = &$traceCommand -I -q 1 -w 1 -n $TargetName 2>&1 | 
+        $trStrings = &$traceCommand -I -q 1 -w 1 -n $TargetName 2>&1 |
         Where-Object { $_ -match '^\s?\d+\s+' }
     }
 
@@ -444,12 +444,12 @@ function Invoke-PingPath {
             }
             Write-Verbose -Message "Testing: $target ..."
             $ping = Test-Connection -TargetName $escapedTarget -Count $Count
-            $results = $ping | group-object -Property status  
+            $results = $ping | group-object -Property status
             $successPings = ($results | Where-Object { $_.name -eq 'success' }).Count
             $successRate = [float]$successPings / $Count
             $lossRate = 1 - $successRate
-            $resolvedTarget = dig -x $target +noall +answer +nocomments | 
-            Where-Object { $_ -match '^[^;]' } | 
+            $resolvedTarget = dig -x $target +noall +answer +nocomments |
+            Where-Object { $_ -match '^[^;]' } |
             ForEach-Object { ($_ -split '[ \t]')[4] }
             [PingPathResult]@{
                 Target         = $target
@@ -465,4 +465,46 @@ function Invoke-PingPath {
         }
         $doneCount++
     }
+}
+
+#https://www.michev.info/blog/post/2140/decode-jwt-access-and-id-tokens-via-powershell
+
+function ConvertFrom-JWTtoken {
+
+    [cmdletbinding()]
+    param([Parameter(Mandatory=$true)][string]$token)
+
+    #Validate as per https://tools.ietf.org/html/rfc7519
+    #Access and ID tokens are fine, Refresh tokens will not work
+    if (!$token.Contains(".") -or !$token.StartsWith("eyJ")) {
+        Write-Error "Invalid token" -ErrorAction Stop
+    }
+
+    #Header
+    $tokenheader = $token.Split(".")[0].Replace('-', '+').Replace('_', '/')
+    #Fix padding as needed, keep adding "=" until string length modulus 4 reaches 0
+    while ($tokenheader.Length % 4) { Write-Verbose "Invalid length for a Base-64 char array or string, adding ="; $tokenheader += "=" }
+    Write-Verbose "Base64 encoded (padded) header:"
+    Write-Verbose $tokenheader
+    #Convert from Base64 encoded string to PSObject all at once
+    Write-Verbose "Decoded header:"
+    [System.Text.Encoding]::ASCII.GetString([system.convert]::FromBase64String($tokenheader)) | ConvertFrom-Json | fl | Out-Default
+
+    #Payload
+    $tokenPayload = $token.Split(".")[1].Replace('-', '+').Replace('_', '/')
+    #Fix padding as needed, keep adding "=" until string length modulus 4 reaches 0
+    while ($tokenPayload.Length % 4) { Write-Verbose "Invalid length for a Base-64 char array or string, adding ="; $tokenPayload += "=" }
+    Write-Verbose "Base64 encoded (padded) payoad:"
+    Write-Verbose $tokenPayload
+    #Convert to Byte array
+    $tokenByteArray = [System.Convert]::FromBase64String($tokenPayload)
+    #Convert to string array
+    $tokenArray = [System.Text.Encoding]::ASCII.GetString($tokenByteArray)
+    Write-Verbose "Decoded array in JSON format:"
+    Write-Verbose $tokenArray
+    #Convert from JSON to PSObject
+    $tokobj = $tokenArray | ConvertFrom-Json
+    Write-Verbose "Decoded Payload:"
+
+    return $tokobj
 }
